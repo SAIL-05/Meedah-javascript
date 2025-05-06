fb77acda71ad648daa2c76046f3693ee6b30e223
Hameedahlawal <lawalhameedah87@gmail.com>
2025-05-06 02:05:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,121 @@
+
+let myAge = 55;
+ myAge = 20;
+
+ console.log(myAge);
+//  let is a cheater
+
+//  const yourAge = 67;
+//  yourAge = 45;
+//  console.log(yourAge);
+//  const is loyal
+
+ let age;
+ age = 45;
+ console.log(age);
+
+// //Primitive data types
+
+//  string '' ""
+
+ let hameedah;
+ hameedah = "oh my shayla,happy birthday boo";
+ console.log(hameedah);
+
+
+// // number
+
+ let myNum = 5;
+ myNum = 12;
+ console.log(myNum);
+//   it assigns just number and not text
+
+// //null
+let test2 = null;
+
+// test2 = 56;
+console.log(test2);
+
+// // Non-primitive data types
+// // objects
+
+ const student = {
+   name: "blessing",
+   age: 56,
+   isNotPregnant: true,
+ };
+
+ console.log(student);
+
+// // arrays
+
+ const myArray = ["Chinbuzo", "Chinyere", 354, false, { name: "Falila" }, [2, 4, 6]];
+ console.log(myArray[0]);
+ console.log(myArray[1]);
+ console.log(myArray[4]);
+
+// // functions
+
+//Arithemetic Operator
+
+  let a = 20;
+  let b = 56;
+
+
+// //Modulus
+ const modul = b % a;
+ console.log(modul);
+
+// //Assignment Operators
+//  let c = 39
+//  +=
+// c += 23;
+// console.log(c)
+//  -=
+// c -= 20;
+// console.log(c)
+// *=
+// c *= 12;
+// console.log(c);
+
+
+//Logical Operator
+// Logical OR ||
+// Logical NOT !
+// Logiacl AND &
+
+// //Ayo's Admission Details
+const fee = true;
+const uniform = false;
+const textbooks = false;
+const idCard = false;
+
+// const checkOR = fee || !textbooks || uniform || idCard;
+// const checkAND = fee && textbooks && uniform && idCard;
+// const checkAyo = fee && textbooks;
+// const checkAyoAgain = (fee && idCard) || (fee && uniform);
+const checkAyoAgain = (fee && (idCard || uniform));
+console.log(checkAyoAgain);
+
+
+// //Tenary Operator
+// // (contition) ? "" : ""
+
+ const tenTest = 50 > 25 ? "correct" : " wrong";
+ console.log(tenTest);
+
+//Write a program that tells if a user is legible to vote
+
+// let ages = 12;
+// let canVote = ages >= 18 ? "Yes you can" : "No you cant";
+
+// let canVote = ages <= 18 ? "yes" : "no"
+// console.log(canVote);
+
+//  let ages = 60;
+
+//  let voting = ages < 18 ? "You cannot vote" : "You can vote";
+//  console.log(voting);
+
+// let voting = ages < 100 && age >= 18 ? "You can vote" : "You cannot vote";
+//  console.log(voting);
